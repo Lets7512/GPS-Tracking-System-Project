fc113a34e9384f17c027c163da5a3c109546eddb
Omar Ibrahim Sabek <omaribrahimsabek@gmail.com>
2021-06-04 22:33:18 +0200

    added systick function

--- Keil Project/main.c
+++ Keil Project/main.c
@@ -2,3 +2,32 @@
 #include "tm4c123gh6pm.h"
 #include "math.h"
 #include "string.h"
+//---------------------------------------------------------------------------
+                        //Global Variables to be used
+												
+												
+												
+//---------------------------------------------------------------------------
+                        //Function declaration
+//---------------------------------------------------------------------------
+void Systick_init(void);
+
+
+//--------------------------------------------------------------------------
+                        // Main Function
+int main(){
+	
+}
+
+
+
+
+//--------------------------------------------------------------------------
+                        //Function Definition
+//System Clock
+void Systick_init(void){
+        NVIC_ST_RELOAD_R = (int)(15999999/1000000);  // for us delay
+				//NVIC_ST_RELOAD_R = 16;  // for us delay
+        NVIC_ST_CTRL_R = 5;
+        NVIC_ST_CURRENT_R = 0;
+}
---
+++ Keil Project/main.d
@@ -0,0 +1,3 @@
+./main.o: main.c ..\Headers\tm4c123gh6pm.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
