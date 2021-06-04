55ff2b706a011607a89d8e3cadcddcd9abd061c0
Omar Ibrahim Sabek <omaribrahimsabek@gmail.com>
2021-06-04 23:21:02 +0200

    added delay us function

--- Keil Project/main.c
+++ Keil Project/main.c
@@ -21,6 +21,7 @@ void portA(void);
 //---------------------------------------------------------------------------
 void Systick_init(void);
 void delay_ms(int i);
+void delay_us(int i);
 //---------------------------------------------------------------------------
 void update_7_segment(void);
 void send_dist_to_segment(void);
@@ -134,10 +135,19 @@ void Systick_init(void){
         NVIC_ST_CTRL_R = 5;
         NVIC_ST_CURRENT_R = 0;
 }
-//Delay function to be used
+//Delay in ms function to be used
 void delay_ms(int i){
     int counter = 0;
     while(counter != i*1000){
+        if(NVIC_ST_CTRL_R & 0x10000){
+            counter++;
+        }
+    }
+}
+//Delay in us function to be used
+void delay_us(int i){
+    int counter = 0;
+    while(counter != i){
         if(NVIC_ST_CTRL_R & 0x10000){
             counter++;
         }
