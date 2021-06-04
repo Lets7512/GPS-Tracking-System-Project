d3aa2c5e8a667372fac74fa7558679fdfcae9fda
Caroline744 <carolinegirgis20@gmail.com>
2021-06-04 22:42:25 +0200

    added update 7 segment function

--- Keil Project/main.c
+++ Keil Project/main.c
@@ -14,6 +14,7 @@ char dist_buffer[16];
 //---------------------------------------------------------------------------
 void Systick_init(void);
 
+void update_7_segment(void);
 
 //--------------------------------------------------------------------------
                         // Main Function
@@ -26,6 +27,17 @@ int main(){
 
 //--------------------------------------------------------------------------
                         //Function Definition
+//Update 7 Segment Readings
+void update_7_segment(void){   
+	int count = (int)dist_travelled - (int)old_dist;
+    	int i = 0;
+    	if(dist_travelled > old_dist){
+        	for(i=0; i < count; i++){
+            		send_dist_to_segment();
+        	}
+        	old_dist = dist_travelled;
+    	}
+}
 //System Clock
 void Systick_init(void){
         NVIC_ST_RELOAD_R = (int)(15999999/1000000);  // for us delay
