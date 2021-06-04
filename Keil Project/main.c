a909d22f89d499d4902d477a4487f157776e8e80
Yousef497 <yousef.shalaby70@gmail.com>
2021-06-04 22:53:52 +0200

    Added PortF Initialize Function

--- Keil Project/main.c
+++ Keil Project/main.c
@@ -12,6 +12,7 @@ char dist_buffer[16];
 //---------------------------------------------------------------------------
                         //Function declaration
 //---------------------------------------------------------------------------
+void portF(void);
 void Systick_init(void);
 
 void update_7_segment(void);
@@ -30,6 +31,21 @@ int main(){
 
 //--------------------------------------------------------------------------
                         //Function Definition
+
+//PortF Intialize to use with Tiva Board LEDs
+void portF(void){
+        SYSCTL_RCGCGPIO_R |= 0x20;                      // enable clock to PORTF
+        while((SYSCTL_PRGPIO_R & 0x20) == 0){}          // waiting for port F stabilization
+        GPIO_PORTF_LOCK_R = 0x4C4F434B;                 // unlock port F
+        GPIO_PORTF_CR_R |= 0x0E;                        // unlock port F
+        GPIO_PORTF_DIR_R |= 0x0E;                       // output pins LEDs
+        GPIO_PORTF_DEN_R |= 0x0E;                       // Digital Pins
+        GPIO_PORTF_AMSEL_R &= ~0x0E;                    // Analog select ; clear 3 pins of LEDs of analog property
+        GPIO_PORTF_AFSEL_R &= ~0x0E;                    // Alternative function or General Purpose Selection; here we selected GPIO
+        GPIO_PORTF_PCTL_R &= ~0x0000FFF0;               // Definition of the alternative function of the pin; here we clear the leds pins
+        GPIO_PORTF_DATA_R &= ~0x0E;                     // clear data from LEDs.
+}
+
 //Update 7 Segment Readings
 void update_7_segment(void){   
 	int count = (int)dist_travelled - (int)old_dist;
