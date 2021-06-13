fe08432ad85228f3d169265b3ac9ea25ff381952
Omar Ibrahim Sabek <omaribrahimsabek@gmail.com>
2021-06-13 23:47:26 +0200

    Update gps.c

--- Source Code/Phase2/gps.c
+++ Source Code/Phase2/gps.c
@@ -1,3 +1,5 @@
+#ifndef GPS_FUNCS_H
+#define GPS_FUNCS_H
 void Process_GGA(char raw_data[]);
 void parse_GGA(char GGA_data[]);
 void update_GPS_vars(void);
@@ -10,6 +12,8 @@ void Process_GLL(char raw_data[]);
 void parse_GLL(char GLL_data[]);
 void Process_RMC(char raw_data[]);
 void parse_RMC(char RMC_data[]);
+void update_time(void);
+#endif
 //----------------------------------------------
 
 enum RMC_index{tm_RMC, stat_RMC, lat_RMC, lat_dir_RMC, long_RMC,
@@ -241,4 +245,37 @@ void parse_GLL(char GLL_data[]){
 	for (i = 0; i < size; i++){
         strcpy(GLL_data_parsed[i], data[i]);
     }
+}
+void update_time(void)
+{
+	
+	char time_arr[10];
+	char tm_h[2];
+	char tm_m[2];
+	char tm_s[5];
+	int rmc_utc_time,gll_utc_time,gga_utc_time;
+	//UART0_WriteString("time udpate started");
+	rmc_utc_time = atoi(RMC_data_parsed[rmc_tm]);
+	//gll_utc_time = atoi(GLL_data_parsed[rmc_tm]);
+	//gga_utc_time = atoi(GGA_data_parsed[rmc_tm]);
+	//sprintf(time_buffer,"%d:%d:%d",gll_utc_time,gga_utc_time,rmc_utc_time);
+	//LCD_Print(time_buffer,0);
+		strcpy(time_arr,RMC_data_parsed[rmc_tm]);
+	/*
+	else if(gll_utc_time >= rmc_utc_time && rmc_utc_time >= gga_utc_time)
+	{
+		//UART0_WriteString("GLL time Caughted");
+		strcpy(time_arr,GLL_data_parsed[rmc_tm]);
+	}
+	else if(gga_utc_time >= rmc_utc_time && rmc_utc_time >= gll_utc_time)
+	{
+		strcpy(time_arr,GGA_data_parsed[rmc_tm]);
+	}*/
+	
+	strncpy(tm_h, time_arr,2);
+	strncpy(tm_m, time_arr+2,2);
+	strncpy(tm_s, time_arr+4,5);
+	time_hours = atoi(tm_h);
+	time_minutes = atoi(tm_m);
+	time_seconds = atof(tm_s);
 }
