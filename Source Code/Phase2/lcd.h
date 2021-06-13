5e09216cf2eda4783877efe353f3ec3a0ee56a6f
Ali-khattab <84637065+Ali-khattab@users.noreply.github.com>
2021-06-13 12:58:11 -0700

    LCD_send_comand_func

--- Source Code/Phase2/lcd.h
+++ Source Code/Phase2/lcd.h
@@ -2,6 +2,7 @@
 #define LCD_FUNCS_H
 
 void LCD_Init(void);
+void LCD_Send_Command(unsigned char choice);
 
 #endif
 
@@ -24,4 +25,13 @@ void LCD_Init(void){
     delay_us(40);
     LCD_Send_Command(x5x7_chars_8bit);
     delay_us(40);
+}
+
+//LCD Send Command
+void LCD_Send_Command(unsigned char choice){
+    GPIO_PORTA_DATA_R = 0; // Making RS and R/W with 0
+    GPIO_PORTB_DATA_R = choice; // Sending Command
+    GPIO_PORTA_DATA_R |= 0x80;  // Making the Latch From Low to High
+    delay_us(40);
+    GPIO_PORTA_DATA_R = 0;  // Making the Latch From Low to High
 }
