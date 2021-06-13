f95cd923b16b1e6341b7fc2b065859736fb2b5cc
Yousef497 <yousef.shalaby70@gmail.com>
2021-06-13 22:14:17 +0200

    Added UART0 Serial Available, Read and Write for characters

--- Source Code/Phase2/uart.c
+++ Source Code/Phase2/uart.c
@@ -2,6 +2,9 @@
                         //Function declaration
 //---------------------------------------------------------------------------
 void UART0_Init(void);
+unsigned char UART0_Available(void);
+unsigned char UART0_Read(void);
+void UART0_Write(unsigned char data);
 //-------------------------------------------------------------------------------
 				//UART Initialize
 //-------------------------------------------------------------------------------
@@ -21,4 +24,21 @@ void UART0_Init(void){
         GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R &~0xFF) | (GPIO_PCTL_PA1_U0TX | GPIO_PCTL_PA0_U0RX);
         GPIO_PORTA_DEN_R |= 0x03;
         GPIO_PORTA_AMSEL_R &= ~0x03;
+}
+
+//UART0 Serial available function
+unsigned char UART0_Available(void){
+    return ((UART0_FR_R & UART_FR_RXFE) == UART_FR_RXFE) ? 0 : 1;
+}
+
+//UART0 Serial Read Function -- Read
+unsigned char UART0_Read(void){
+        while(UART0_Available() != 1);
+        return (unsigned char)(UART0_DR_R & 0xFF);
+}
+
+//UART0 Serial Write Function -- Transmit
+void UART0_Write(unsigned char data){
+        while((UART0_FR_R & UART_FR_TXFF) != 0);
+        UART0_DR_R = data;
 }
