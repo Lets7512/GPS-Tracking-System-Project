323009ab1a99d2420540b0aff7cf861a32e14086
Yousef497 <yousef.shalaby70@gmail.com>
2021-06-13 22:22:01 +0200

    Added UART0 Read and write strings functions

--- Source Code/Phase2/uart.c
+++ Source Code/Phase2/uart.c
@@ -5,10 +5,12 @@ void UART0_Init(void);
 unsigned char UART0_Available(void);
 unsigned char UART0_Read(void);
 void UART0_Write(unsigned char data);
+void UART0_ReadString(char *str, char startCh , char stopCh);
+void UART0_WriteString(char *str);
 //-------------------------------------------------------------------------------
 				//UART Initialize
 //-------------------------------------------------------------------------------
-//UART0 Initialize
+//UART0 Initialize -------- To test data recieved on PC
 void UART0_Init(void){
         SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0;
         SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;
@@ -41,4 +43,25 @@ unsigned char UART0_Read(void){
 void UART0_Write(unsigned char data){
         while((UART0_FR_R & UART_FR_TXFF) != 0);
         UART0_DR_R = data;
+}
+
+//UART0 Write string
+void UART0_WriteString(char *str){                              //Write String
+  while(*str){
+    UART0_Write(*str);
+    str++;
+  }
+}
+
+//UART0 Read String
+void UART0_ReadString(char *str, char startCh , char stopCh){      //Read String
+    char c = UART0_Read();
+    if (c == startCh){
+    	while(c && c != stopCh){
+        *str = c;
+        str++;
+        c = UART0_Read();
+    	}
+    }
+    *str = 0x00;
 }
